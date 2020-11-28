6c7300e222b4fe0a08855f9e3dd296bdf8706312
motri <mahmoud.otri@senacor.com>
2020-11-28 21:55:13 +0100

    configure try again button

--- thermalApp/app/src/main/java/com/example/thermalapp/OfflineActivity.java
+++ thermalApp/app/src/main/java/com/example/thermalapp/OfflineActivity.java
@@ -1,13 +1,29 @@
 package com.example.thermalapp;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class OfflineActivity extends AppCompatActivity {
 
+    Button btn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_offline);
+
+        btn = findViewById(R.id.btn_off);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(OfflineActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 }
