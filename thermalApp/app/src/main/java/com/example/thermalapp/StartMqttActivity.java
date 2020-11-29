da515a5b72a1df77451d30242a0e7a6331fec27e
motri <mahmoud.otri@senacor.com>
2020-11-29 03:57:01 +0100

    configure toast msg and transferring wifi info properly

--- thermalApp/app/src/main/java/com/example/thermalapp/StartMqttActivity.java
+++ thermalApp/app/src/main/java/com/example/thermalapp/StartMqttActivity.java
@@ -1,13 +1,90 @@
 package com.example.thermalapp;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.net.wifi.WifiInfo;
+import android.net.wifi.WifiManager;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.math.BigInteger;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.nio.ByteOrder;
+
 public class StartMqttActivity extends AppCompatActivity {
+
+    private ImageButton imgBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_mqtt);
+
+        imgBtn = findViewById(R.id.btn_info);
+        imgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(StartMqttActivity.this, MainActivity.class);
+                startActivity(intent);
+                showToast();
+                finish();
+            }
+        });
     }
+
+
+    public void showToast(){
+        LayoutInflater layoutInflater = getLayoutInflater();
+        View layout = layoutInflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_root));
+
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        WifiInfo wifiInfo = wifiManager.getConnectionInfo();
+        String ipAddr = wifiIpAddress(getApplicationContext());
+        String ssid = wifiInfo.getSSID();
+
+        TextView textView = layout.findViewById(R.id.toast_txt);
+        textView.setText("Ip Address: " +  ipAddr + "\n" + "Wifi SSID: " + ssid);
+
+        textView.setTextColor(Color.WHITE);
+
+        Toast toast = new Toast(getApplicationContext());
+        textView.setGravity(Gravity.CENTER);
+        toast.setDuration(500);
+        toast.setView(layout);
+
+        toast.show();
+    }
+
+    private String wifiIpAddress(Context applicationContext) {
+        WifiManager wifiManager = (WifiManager) getSystemService(WIFI_SERVICE);
+        int ipAddress = wifiManager.getConnectionInfo().getIpAddress();
+
+        // Convert little-endian to big-endianif needed
+        if (ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN)) {
+            ipAddress = Integer.reverseBytes(ipAddress);
+        }
+
+        byte[] ipByteArray = BigInteger.valueOf(ipAddress).toByteArray();
+
+        String ipAddressString;
+        try {
+            ipAddressString = InetAddress.getByAddress(ipByteArray).getHostAddress();
+        } catch (UnknownHostException ex) {
+            Log.e("WIFIIP", "Unable to get host address.");
+            ipAddressString = null;
+        }
+
+        return ipAddressString;
+    }
+
 }
