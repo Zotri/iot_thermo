6a914f67f357a2691faa68015f7348c9f7122dd6
motri <mahmoud.otri@senacor.com>
2021-01-12 10:43:55 +0100

    add more layout elements to fruit the UI. Enable MQTT publish methods. HTTP Client is set

--- thermalApp/app/src/main/java/com/example/thermalapp/StartTempCheckActivity.java
+++ thermalApp/app/src/main/java/com/example/thermalapp/StartTempCheckActivity.java
@@ -8,23 +8,41 @@ import android.widget.NumberPicker;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.*;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 public class StartTempCheckActivity extends AppCompatActivity implements MqttCallbackExtended, NumberPicker.OnValueChangeListener {
+    public static String TEMPVALUE;
     final String BROKER_URL = "tcp://192.168.1.113:1883";
-    final String TOPIC = "tele/temp/SENSOR";
+    final String SENSOR_TOPIC = "tele/temp/SENSOR";
+    final String SWITCH_POWER_STAT_TOPIC = "stat/temp/POWER";
     final String USERNAME = "motri";
     final String PASSWORD = "motri";
     final String CLIENT_ID = "android " + MqttClient.generateClientId();
+    final String SENSOR_KEY = "SENSOR";
+    final String POWER_KEY = "POWER";
+    final String POWER_OFF = "POWEROFF";
+    final String POWER_ON = "POWERON";
     public MqttAndroidClient mqttAndroidClient;
-    TextView dataReceived;
-    TextView topicSelected;
-    Button connectMqttBtn;
+    MemoryPersistence persistence = new MemoryPersistence();
+    TextView sensor_dataReceived;
+    TextView power_dataReceived;
+    TextView topicSensorSelected;
+    TextView topicPowerSelected;
     TextView tvShowNumberPicker;
+    Button connectMqttBtn;
+    Button switchOffPowerBtn;
+    Button switchONPowerBtn;
+    Button setTemperatureBtn;
     NumberPickerWithXml numberPicker;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,45 +51,103 @@ public class StartTempCheckActivity extends AppCompatActivity implements MqttCal
 
         tvShowNumberPicker = findViewById(R.id.show_number_picker_text);
         numberPicker = findViewById(R.id.temp_degree_picker);
-
         numberPicker.setOnValueChangedListener(this);
-        //numberPicker.setFocusedByDefault(true);
 
-        /*
-        numberPicker.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+        topicPowerSelected = findViewById(R.id.text_topic_power_value);
+        topicSensorSelected = findViewById(R.id.text_topic_sensor_value);
+
+        setTemperatureBtn = findViewById(R.id.btn_set_temp);
+
+        switchONPowerBtn = findViewById(R.id.btn_switch_power_on);
+        switchONPowerBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onFocusChange(View view, boolean b) {
-                //publish();
+            public void onClick(View view) {
+                sendMqttCmnd("http://192.168.1.118/cm?cmnd=Power%20On", POWER_ON);
             }
         });
-*/
+
+        switchOffPowerBtn = findViewById(R.id.btn_switch_power_off);
+        switchOffPowerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sendMqttCmnd("http://192.168.1.118/cm?cmnd=Power%20off", POWER_OFF);
+            }
+        });
+
         connectMqttBtn = findViewById(R.id.btn_mqtt_connect);
         connectMqttBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent(StartTempCheckActivity.this, StartTempCheckActivity.class);
-                //startActivity(intent);
                 startMqtt();
-                //finish();
             }
         });
     }
 
-    private void publish() {
-        int settedValue = numberPicker.getValue();
+    private void sendMqttCmnd(String url, final String key) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest request = new JsonObjectRequest(url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        switch (key) {
+                            case SENSOR_KEY:
+                                try {
+                                    String tempValue = response.getJSONObject("StatusSNS").toString();
+                                    mqttAndroidClient.publish(SENSOR_TOPIC, tempValue.getBytes(), 0, true);
+                                    Log.d("response from sensor", response.toString());
+                                } catch (MqttException | JSONException e) {
+                                    e.printStackTrace();
+                                    Log.d("response", response.toString());
+                                }
+                            case POWER_KEY:
+                                try {
+                                    String powerValue = response.getJSONObject("POWER").toString();
+                                    mqttAndroidClient.publish(SWITCH_POWER_STAT_TOPIC, powerValue.getBytes(), 0, true);
+                                    Log.d("response from power switch", powerValue);
+                                } catch (MqttException | JSONException e) {
+                                    e.printStackTrace();
+                                    Log.d("response", response.toString());
+                                }
+                            case POWER_OFF:
+                                try {
+                                    String powerOffCmnd = response.getJSONObject("POWER").toString();
+                                    mqttAndroidClient.publish(SWITCH_POWER_STAT_TOPIC, powerOffCmnd.getBytes(), 0, true);
+                                    Log.d("response from power switch", powerOffCmnd);
+                                } catch (MqttException | JSONException e) {
+                                    e.printStackTrace();
+                                    Log.d("response", response.toString());
+                                }
+                            case POWER_ON:
+                                try {
+                                    String powerOnCmnd = response.getJSONObject("POWER").toString();
+                                    mqttAndroidClient.publish(SWITCH_POWER_STAT_TOPIC, powerOnCmnd.getBytes(), 0, true);
+                                    Log.d("response from power switch", powerOnCmnd);
+                                } catch (MqttException | JSONException e) {
+                                    e.printStackTrace();
+                                    Log.d("response", response.toString());
+                                }
+                        }
+                    }
+                }, new Response.ErrorListener() {
 
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        queue.add(request);
     }
 
-    private void startMqtt() {
-        dataReceived = findViewById(R.id.text_temp_value);
-        topicSelected = findViewById(R.id.text_topic_value);
+    ;
 
-        mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), BROKER_URL, CLIENT_ID);
-        byte[] payload = "Android logs out from broker...".getBytes();
+    private void startMqtt() {
+        sensor_dataReceived = findViewById(R.id.text_temp_value);
+        power_dataReceived = findViewById(R.id.text_power_value);
+
+        mqttAndroidClient = new MqttAndroidClient(this.getApplicationContext(), BROKER_URL, CLIENT_ID, persistence);
         MqttConnectOptions options = new MqttConnectOptions();
         options.setAutomaticReconnect(true);
         options.setMqttVersion(MqttConnectOptions.MQTT_VERSION_3_1);
-        options.setWill(TOPIC, payload, 0, false);
         options.setUserName(USERNAME);
         options.setPassword(PASSWORD.toCharArray());
 
@@ -80,33 +156,35 @@ public class StartTempCheckActivity extends AppCompatActivity implements MqttCal
             token.setActionCallback(new IMqttActionListener() {
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
+                    subscribe(mqttAndroidClient, SENSOR_TOPIC);
+                    subscribe(mqttAndroidClient, SWITCH_POWER_STAT_TOPIC);
 
-                    subscribe(mqttAndroidClient, TOPIC);
                     mqttAndroidClient.setCallback(new MqttCallback() {
                         @Override
                         public void connectionLost(Throwable cause) {
-
+                            Log.d("mqtt", "connectionLost");
                         }
 
                         @Override
                         public void messageArrived(String topic, MqttMessage message) throws Exception {
-                            String payloadToJson = new JSONObject(String.valueOf(message)).toString();
-                            Log.d("temperature", payloadToJson);
+                            if (topic.equals(SENSOR_TOPIC)) {
+                                String payloadToJson = new JSONObject(String.valueOf(message)).toString();
+                                JSONObject json = new JSONObject(payloadToJson);
+                                JSONObject location = json.getJSONObject("DS18B20");
+                                TEMPVALUE = location.getString("Temperature");
 
+                                Log.d("temperature value", TEMPVALUE + "°");
+                                sensor_dataReceived.setText(TEMPVALUE + "\u2103");
 
-                            JSONObject json = new JSONObject(payloadToJson);
-
-                            JSONObject location = json.getJSONObject("DS18B20");
-                            String temp = location.getString("Temperature");
-
-                            Log.d("temperature", temp + "°");
-                            dataReceived.setText(temp + "\u2103");
-
+                            } else if (topic.equals(SWITCH_POWER_STAT_TOPIC)) {
+                                Log.d("Power", new String(message.getPayload()));
+                                power_dataReceived.setText("Thermostat is " + new String(message.getPayload()));
+                            }
                         }
 
                         @Override
                         public void deliveryComplete(IMqttDeliveryToken token) {
-
+                            Log.d("mqtt", "deliveryComplete");
                         }
                     });
                     // We are connected
@@ -142,17 +220,24 @@ public class StartTempCheckActivity extends AppCompatActivity implements MqttCal
         Log.d("MQTT", "deliveryComplete");
     }
 
-    public void subscribe(MqttAndroidClient client, String topic) {
-        int qos = 1;
+    public void subscribe(MqttAndroidClient client, final String topic) {
+        int qos = 0;
         try {
             IMqttToken subToken = client.subscribe(topic, qos);
             subToken.setActionCallback(new IMqttActionListener() {
 
                 @Override
                 public void onSuccess(IMqttToken asyncActionToken) {
-                    // The message was published
-                    Log.w("Mqtt", "Subscribed!");
-                    topicSelected.setText("Topic selected!");
+                    if (topic.equals("tele/temp/SENSOR")) {
+                        sendMqttCmnd("http://192.168.1.117/cm?cmnd=status%2010", SENSOR_KEY);
+
+                        // The message was published
+                        Log.w("Mqtt", "SENSOR Subscribed!");
+                        topicSensorSelected.setText("Topic SENSOR selected!");
+                    } else
+                        sendMqttCmnd("http://192.168.1.118/cm?cmnd=Power", POWER_KEY);
+                    Log.w("Mqtt", "POWER Subscribed!");
+                    topicPowerSelected.setText("Topic POWER selected!");
                 }
 
                 @Override
@@ -173,9 +258,39 @@ public class StartTempCheckActivity extends AppCompatActivity implements MqttCal
     }
 
     @Override
-    public void onValueChange(NumberPicker numberPicker, int i, int i1) {
-        Log.d("old", i + "°" + "new" + i1);
-        tvShowNumberPicker.setText(" \u2103 degree to set");
+    public void onValueChange(NumberPicker numberPicker, int oldValue, int chosenValue) {
+        Log.d("old", oldValue + "°" + "new" + chosenValue);
+        tvShowNumberPicker.setText(chosenValue + " \u2103 degree to set. Click on SET to proceed!");
+
+        try {
+            Log.d("TEMPVALUE", "value before conversion " + TEMPVALUE);
+            float convertedTemp = Float.parseFloat(TEMPVALUE);
+            Log.d("integer static converted", "temp " + convertedTemp);
+            if (Math.round(convertedTemp) <= chosenValue) {
+                setTemperatureBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        sendMqttCmnd("http://192.168.1.118/cm?cmnd=Power%20On", POWER_ON);
+                        Toast.makeText(getBaseContext(), "Thermostat is set", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else if (Math.round(convertedTemp) >= chosenValue) {
+                setTemperatureBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+
+                        sendMqttCmnd("http://192.168.1.118/cm?cmnd=Power%20off", POWER_OFF);
+                        Toast.makeText(getBaseContext(), "Thermostat is off, temperature is satisfying", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            } else if (Math.round(convertedTemp) > 27) {
+                sendMqttCmnd("http://192.168.1.118/cm?cmnd=Power%20off", POWER_OFF);
+                Toast.makeText(getBaseContext(), "Thermostat is off, temperature is too high", Toast.LENGTH_SHORT).show();
+            }
+        } catch (NumberFormatException e) {
+            Log.d("Error", "temp convert error");
+        }
     }
     //TODO implement publish method
     // publish to subscribe with topic tele/%topic%/STATUS to retrieve a payload with least info as json
